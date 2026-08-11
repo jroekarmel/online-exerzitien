95da1fb805ade961bdc1c36258c539d023cb27f5
jroekarmel <j.roe@karmel.at>
2026-08-11 10:32:44 +0200

    added weeks loading for fs26

---
+++ exerzitien-archiv/de-fs-26-script.js
@@ -0,0 +1,35 @@
+let fs26Data;
+const weeksGrid = document.querySelector('#weeks-grid');
+let titleText;
+
+async function loadData(){
+    let resp = await fetch('de-inhalt-fs26.json');
+    fs26Data = await resp.json();
+}
+
+function displayWeeks(){
+  const weeks = fs26Data.woches;
+    titleText ='';
+    weeks.forEach(item => {
+    
+        titleText += `
+        <article class="archive-card">
+        <div class="image-zoom">
+          <img
+            src="${item.bild_url}"
+            loading="lazy"
+          >
+        </div>
+
+        <h3>${item.title}</h3>
+
+        <p class="archive-theme">
+          ${item.kurz_text}
+        </p>
+      </article>
+        `;
+    });
+    weeksGrid.innerHTML = titleText;
+}
+loadData()
+    .then(displayWeeks)
