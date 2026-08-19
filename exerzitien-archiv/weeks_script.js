50cfe64dececbbfe95b2a9d7bd5ad089939697b0
JoshR <j.roe@karmel.at>
2026-08-19 10:42:25 +0200

    removing video

--- exerzitien-archiv/weeks_script.js
+++ exerzitien-archiv/weeks_script.js
@@ -39,19 +39,6 @@ function displayWeeks(data){
             Materialien dieses Impulses
           </a>
         </div>
-
-        
-        ${videoEmbedUrl ? `
-          <div class="card-video">
-            <iframe
-              src="${videoEmbedUrl}"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            ></iframe>
-          </div>
-        ` : ''}
       </article>
         `;
     });
