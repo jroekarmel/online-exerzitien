b9246fc83aa9e948ea2d0ebc06ed7a0b013d7d59
JoshR <j.roe@karmel.at>
2026-09-08 13:57:59 +0200

    adjusting link

--- de/heilige/heiligen_script.js
+++ de/heilige/heiligen_script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (!container) return;
 
-  const DATA_URL = "./data/exerzitien-katalog.json";
+  const DATA_URL = "./data/exerzitien/exerzitien-katalog.json";
 
   console.log (DATA_URL)
   const seasonLabels = {
