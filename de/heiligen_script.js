79008f7851d2816015bcd7630d5d5d97fb15d0bc
JoshR <j.roe@karmel.at>
2026-09-08 15:36:03 +0200

    updating json location

--- de/heiligen_script.js
+++ de/heiligen_script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (!container) return;
 
-  const DATA_URL = "../online-exerzitien/data/retreat_overview.json";
+  const DATA_URL = "../de/data/exerzitien-katalog.json";
 
   console.log (DATA_URL)
   const seasonLabels = {
